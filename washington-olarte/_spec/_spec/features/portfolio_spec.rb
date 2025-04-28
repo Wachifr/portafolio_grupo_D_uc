215d5225f7a1bf1fe44f89e3b87f7e8ff0b2c664
Washington olarte <23976963@continental.edu.pe>
2025-04-27 21:24:51 -0500

    Create portfolio_spec.rb

---
+++ washington-olarte/_spec/_spec/features/portfolio_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe 'Portfolio', type: :feature do
+  before { visit '/portfolio.html' }
+
+  it 'muestra los proyectos del JSON' do
+    expect(page).to have_content('Proyecto 1')
+    expect(page).to have_content('Ruby')
+    expect(page).to have_content('Proyecto 2')
+  end
+end
